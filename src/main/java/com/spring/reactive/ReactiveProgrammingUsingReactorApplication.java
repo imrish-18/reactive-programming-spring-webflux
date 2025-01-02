d8e51618d7772167e453fa7b80d18181cab2c485
rishabh <rish.sharma9012@gmail.com>
2025-01-02 09:58:42 +0530

    adding mongo db commands

--- src/main/java/com/spring/reactive/ReactiveProgrammingUsingReactorApplication.java
+++ src/main/java/com/spring/reactive/ReactiveProgrammingUsingReactorApplication.java
@@ -1,13 +1,20 @@
 package com.spring.reactive;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoCollection;
+import com.mongodb.reactivestreams.client.MongoDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import org.bson.Document;
 
 @SpringBootApplication
 public class ReactiveProgrammingUsingReactorApplication {
@@ -19,8 +26,28 @@ public class ReactiveProgrammingUsingReactorApplication {
 		programming.namesFlux().subscribe(res->System.out.println(res));
 		programming.namesMono().subscribe(res->System.out.println(res));
 		programming.namesFlux_FlatMap().subscribe(res->System.out.println(res));
-		
-		
+
+		try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+			// Access the database
+			MongoDatabase database = mongoClient.getDatabase("MovieInfo");
+
+			// Access the collection
+			MongoCollection<org.bson.Document> collection=database.getCollection("inventory");
+
+			// Insert multiple documents
+			collection.insertMany(Arrays.asList(
+					 org.bson.Document.parse("{ item: 'journal', qty: 25, size: { h: 14, w: 21, uom: 'cm' }, status: 'A' }"),
+					Document.parse("{ item: 'notebook', qty: 50, size: { h: 8.5, w: 11, uom: 'in' }, status: 'A' }"),
+					Document.parse("{ item: 'paper', qty: 100, size: { h: 8.5, w: 11, uom: 'in' }, status: 'D' }"),
+					Document.parse("{ item: 'planner', qty: 75, size: { h: 22.85, w: 30, uom: 'cm' }, status: 'D' }"),
+					Document.parse("{ item: 'postcard', qty: 45, size: { h: 10, w: 15.25, uom: 'cm' }, status: 'A' }")
+			));
+
+			System.out.println("Documents inserted successfully!");
+			System.out.printf("collections is "+collection.find());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public  Flux<String> namesFlux()
